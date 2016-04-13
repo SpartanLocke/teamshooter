7e98f3dfc611d7b32d9e4bbc40b31c49e9e36a0a
Julian Contreras <jrcontre@mit.edu>
2016-04-13 15:49:22 -0400

    controller stuff

---
+++ Assets/Scripts/player_controller_stuff/PlayerUIInputController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerUIInputController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+    // leave the photon lobby
+    // go back to the main controller scene
+    public void onExitButonClicked() {
+        Debug.Log("player clicked exit button");
+        if (PhotonNetwork.connectionStateDetailed == PeerState.Joined) {
+            PhotonNetwork.LeaveRoom();
+        }
+    }
+
+    void OnLeftRoom() {
+        Debug.Log("left the room!");
+    }
+}
