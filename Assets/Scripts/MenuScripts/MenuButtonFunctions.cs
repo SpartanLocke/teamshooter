8624732e2f3361bdbd3c8559538071712bf51b84
Samuel Van Cise <vancisesam@gmail.com>
2016-04-25 15:12:09 -0400

    Menu Shit

---
+++ Assets/Scripts/MenuScripts/MenuButtonFunctions.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuButtonFunctions : MonoBehaviour {
+    public GameObject lobbySelectButtons;
+    public GameObject menuButtons;
+public void openPlayButtons()
+    {
+        menuButtons.SetActive(false);
+        lobbySelectButtons.SetActive(true);
+    }
+public void backFromLobby()
+    {
+        menuButtons.SetActive(true);
+        lobbySelectButtons.SetActive(false);
+    }
+}
