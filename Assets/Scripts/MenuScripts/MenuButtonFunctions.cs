d2079448ce5853dfc9bc3be88c36f7fcb550df9b
Samuel Van Cise <vancisesam@gmail.com>
2016-04-25 18:27:10 -0400

    Sound on UI

--- Assets/Scripts/MenuScripts/MenuButtonFunctions.cs
+++ Assets/Scripts/MenuScripts/MenuButtonFunctions.cs
@@ -6,24 +6,41 @@ public class MenuButtonFunctions : MonoBehaviour {
     public GameObject menuButtons;
     public GameObject UI;
     public GameObject colorSelect;
+    public AudioSource buttonSound;
+    void Start()
+    {
+        buttonSound = gameObject.GetComponent<AudioSource>();
+    }
     public void openPlayButtons()
     {
         menuButtons.SetActive(false);
         lobbySelectButtons.SetActive(true);
+        buttonSound.Play();
     }
     public void backFromLobby()
     {
         menuButtons.SetActive(true);
         lobbySelectButtons.SetActive(false);
+        buttonSound.Play();
     }
     public void toColorSelect()
     {
         UI.SetActive(false);
         colorSelect.SetActive(true);
+        buttonSound.Play();
     }
     public void backFromColorSelect()
     {
         UI.SetActive(true);
         colorSelect.SetActive(false);
+        buttonSound.Play();
+    }
+
+    public void disableAll()
+    {
+        UI.SetActive(false);
+        colorSelect.SetActive(false);
+        lobbySelectButtons.SetActive(false);
+        menuButtons.SetActive(false);
     }
 }
